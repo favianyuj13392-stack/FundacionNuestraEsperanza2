fb60419c94598b6c643e332bb94be45587579c22
Esme <esmeralda.medina.p@ucb.edu.bo>
2026-03-30 10:44:29 -0400

    Cambio en nombre de carpeta y DatabaseSeeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             RolesSeeder::class,
             AdminUserSeeder::class,
             TestUserSeeder::class,
+            SeetingSeeder::class,
         ]);
     }
 }
