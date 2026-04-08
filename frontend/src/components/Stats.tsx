d7b58010e7ec3bbbe74bcceab1a8dc0ed478bd57
Esme <esmeralda.medina.p@ucb.edu.bo>
2026-04-08 19:17:11 -0400

    Stats

--- frontend/src/components/Stats.tsx
+++ frontend/src/components/Stats.tsx
@@ -24,29 +24,15 @@ function Counter({ from, to }: { from: number, to: number }) {
 }
 
 const Stats = () => {
-  const [settings, setSettings] = useState<any>(null);
+  const [stats, setStats] = useState<any[]>([]);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/settings') // Asegúrate de usar tu variable de entorno aquí
+    fetch('http://127.0.0.1:8000/api/stats') 
       .then(res => res.json())
-      .then(data => setSettings(data))
-      .catch(err => console.error("Error cargando settings:", err));
+      .then(data => setStats(data))
+      .catch(err => console.error("Error cargando stats:", err));
   }, []);
 
-  const stats = [
-    { 
-      number: settings?.stats_ninos_ayudados || '0', 
-      text: 'Niños que recibieron ayuda' 
-    },
-    { 
-      number: settings?.stats_diagnosticos_anuales || '0', 
-      text: 'Diagnósticos de niños con cáncer por año en Bolivia' 
-    },
-    { 
-      number: settings?.stats_voluntarias || '0', 
-      text: 'Voluntarias' 
-    },
-  ];
   return (
     <section className="relative py-20">
       <div className="absolute inset-0 z-0">
@@ -57,12 +43,10 @@ const Stats = () => {
           objectFit="cover"
           quality={90}
         />
-        {/* Aumentando la opacidad para mejorar la legibilidad del texto en móviles */}
         <div className="absolute inset-0 bg-black opacity-40"></div>
       </div>
 
       <div className="container mx-auto px-6 relative z-10 text-white text-center">
-        {/* Grid se convierte a 1 columna en móvil y 3 en desktop */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-8">
           {stats.map((stat, index) => (
             <motion.div
@@ -74,9 +58,11 @@ const Stats = () => {
               className="p-4"
             >
               <h3 className="text-5xl md:text-6xl font-extrabold font-title">
-                +<Counter from={0} to={Number(stat.number) || 0} />
+                <Counter from={0} to={Number(stat.number)} />+
               </h3>
-              <p className="mt-2 text-md md:text-lg font-sans">{stat.text}</p>
+              <p className="mt-4 text-xl md:text-2xl font-medium font-sans">
+                {stat.label}
+              </p>
             </motion.div>
           ))}
         </div>
