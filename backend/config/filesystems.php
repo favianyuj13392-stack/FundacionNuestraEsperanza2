44799dbf6d35536fdb02bf44c037b2c0181fad14
Esme <esmeralda.medina.p@ucb.edu.bo>
2026-06-11 21:50:15 -0400

    Error corregido en news, programs y testimonials

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -64,6 +64,7 @@ return [
             'api_key' => env('CLOUDINARY_API_KEY'),
             'api_secret' => env('CLOUDINARY_API_SECRET'),
             'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+            'url'          => env('CLOUDINARY_URL', 'https://res.cloudinary.com/' . env('CLOUDINARY_CLOUD_NAME') . '/image/upload'),
         ],
     ],
 
